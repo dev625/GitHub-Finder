da5eea0971da29972cc8ee35b77717aaaace8a01
Devesh Lohumi <witdx514@gmail.com>
2021-08-28 14:50:52 +0530

    Refactoring to Stateless Components

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
-        <div className="container">
+        <div className='container'>
           <Users />
         </div>
       </div>

0d7ef8883979523a0ee9f4cdc877e6986a0530c2
Devesh Lohumi <witdx514@gmail.com>
2021-08-28 19:20:26 +0530

    Passing Props Up and Search

--- src/App.js
+++ src/App.js
@@ -11,21 +11,31 @@ class App extends Component {
     loading: false,
   }
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true })
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  //     client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   )
+  //   this.setState({ users: res.data, loading: false })
+  // }
+
+  //Search GitHub Users
+  searchUsers = async (text) => {
     this.setState({ loading: true })
+    console.log(text)
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
       client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     )
-    this.setState({ users: res.data, loading: false })
-    console.log(res.data)
+    this.setState({ users: res.data.items, loading: false })
   }
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <Search />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
